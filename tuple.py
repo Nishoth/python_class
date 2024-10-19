8d99152bd2db9cca7eca7207dffe0b4554343a3a
Nishoth <nishothnishan@gmail.com>
2024-10-19 14:53:25 +0530

    tuple class

--- tuple.py
+++ tuple.py
@@ -1,4 +1,31 @@
-a=10
-b=20
-total=a+b
-print(total)
+# almost tuple is like list
+tuble = (1,2,3,5,5,6)
+# tuble.sort()
+print(tuble)
+
+# indexing
+print(tuble[4])
+
+# slicing / to
+print(tuble[2:4])
+print(tuble[-3:]) # last 
+print(len(tuble))
+# print(tuble.append("6"))
+print(tuble)
+
+
+tuble1=("nishoth")
+print(tuble1.upper())
+print(tuble1)
+
+
+# index
+tuble2 = (1,5,3,6,7,8,5)
+print(tuble2.index(8))
+
+# concatination
+tuble3=(10,60,30,12,45,80)
+print(tuble2+tuble3)
+
+# repetation
+print(tuble1*2)
