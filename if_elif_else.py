6976a5c779655a8f6dc3477b92bbf11b61c6f944
Nishoth <nishothnishan@gmail.com>
2024-11-09 06:43:31 +0530

    add multy condition

--- if_elif_else.py
+++ if_elif_else.py
@@ -1,9 +1,12 @@
 # if ,elif else
 age = 20
-if age >= 20:
+if age > 20:
     print("you can vote")
+elif age == 20:
+    print("you can apply for voter ID")
 else:
     print("you can't vote")   
+    
 
 
 
