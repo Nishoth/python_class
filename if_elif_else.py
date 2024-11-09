7e09267e4f1bde1a0e697d411ad9fb7d6ee7d33e
Nishoth <nishothnishan@gmail.com>
2024-11-09 12:24:26 +0530

    elif

--- if_elif_else.py
+++ if_elif_else.py
@@ -4,8 +4,33 @@ if age > 20:
     print("you can vote")
 elif age == 20:
     print("you can apply for voter ID")
+# use the elif condition for multy condition
 else:
-    print("you can't vote")   
+    print("you can't vote")
+
+# And
+a , b = 10 , 20
+if a == 10 and b == 20:
+    print("condition satisfied")
+else:
+    print("condition not satisfied")
+
+# Or
+a , b = 10 , 20
+if a == 10 or b == 5:
+    print("condition satisfied")
+else:
+    print("condition not satisfied")
+
+# nested if
+a , b = 10 , 20
+if a == 10 :
+    print("condition satisfied")
+    if b == 20:
+        print("condition completely satisfied")
+else:
+    print("condition not satisfied")
+
     
 
 
